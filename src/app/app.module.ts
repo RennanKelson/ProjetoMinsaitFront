923d089b0a46689cd918d8b8298acab98a2c5ae8
Rennan Kelson <rennankelson@hotmail.com>
2022-11-14 16:01:54 -0300

    foi criado component livros, service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,27 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatIconModule} from '@angular/material/icon';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LivrosComponent } from './livros/livros.component';
+import { LivrosService } from './livros.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LivrosComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [HttpClientModule, LivrosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
